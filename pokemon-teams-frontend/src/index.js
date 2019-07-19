8bf12f24f9b1b36c2b86e433173b3aa0fce1d355
Claire Wilbert <claire.wilbert@gmail.com>
2019-07-19 10:29:31 -0700

    set up backend

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,20 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', {
+  fetch(TRAINERS_URL).then(response => response.json()).then(json => console.log(json))
+})
+
+function renderTrainer(trainer){
+  let card = document.createElement('div');
+  card.className = 'card'
+  card.setAttribute('data-id', trainer.id);
+  let addPokeButton = document.createElement('button');
+  addPokeButton.setAttribute('data-trainer-id', trainer.id);
+  let pokeList = document.createElement('ul');
+  card.appendChild(addPokeButton);
+  card.appendChild(pokeList);
+  document.querySelector('main').appendChild(card);
+
+}
