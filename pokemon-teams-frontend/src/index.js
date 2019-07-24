4c8bf283823b59f2e23ab062a8531ee288f640a8
github username <github email address>
2019-07-23 22:51:48 -0700

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+you did this with a pair, check your pair projects folder.
